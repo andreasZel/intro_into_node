347c8dfdcdbd59f50f3d8dbfff68b2e0e2beeaf9
Andreas Zelios <a.zelios@satways.net>
2025-02-01 13:33:11 +0200

    created a CRUID for notes using dB methods

---
+++ src/notes.js
@@ -0,0 +1,48 @@
+import { getDB, saveDB, insert, insertDB } from "./db";
+
+export const newNote = async (note, tags) => {
+    const newNote = {
+        tags,
+        content: note,
+        id: Date.now()
+    }
+
+    await insertDB(newNote);
+
+    return newNote;
+}
+
+export const getAllNotes = async () => {
+
+    const { notes } = await getDB();
+
+    return notes;
+}
+
+export const findNotes = async (filter) => {
+
+    const fetchednotes = await getAllNotes();
+    const notes = fetchednotes.filter((note) => note.content.toLowerCase().includes(filter.toLowerCase()));
+
+    return notes;
+}
+
+export const deleteNote = async (noteId) => {
+
+    const fetchednotes = await getAllNotes();
+    const note = fetchednotes.find((note) => note.id === noteId);
+
+    if (note) {
+        const newNotes = fetchednotes.filter((note) => note.id !== noteId);
+        await saveDB({
+            notes: newNotes
+        });
+
+        return id;
+    }
+
+}
+
+export const removeAllNotes = async () => {
+    await saveDB({ notes: [] })
+}
