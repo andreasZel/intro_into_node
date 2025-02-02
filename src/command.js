0f7788837a7b9885337f84b4dbd2118bed2dd023
Andreas Zelios <a.zelios@satways.net>
2025-02-02 13:46:34 +0200

    used server in yarg

--- src/command.js
+++ src/command.js
@@ -1,6 +1,7 @@
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
 import { getAllNotes, newNote, removeAllNotes, deleteNote, findNotes } from './notes.js';
+import { start } from './server.js'
 
 const listNotes = (notes) => {
     notes.forEach(note => {
@@ -60,7 +61,8 @@ yargs(hideBin(process.argv))
                 type: 'number'
             })
     }, async (argv) => {
-
+        const notes = await getAllNotes();
+        start(notes, argv.port)
     })
     .command('clean', 'remove all notes', () => { }, async (argv) => {
         await removeAllNotes();
